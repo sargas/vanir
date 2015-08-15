1d65e54712c4f349380aa464c2929c0a6356aa73
Joseph Jon Booker <joe@neoturbine.net>
2015-08-14 23:23:12 -0400

    Add "Computational Methods" catagory

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var { Panel } = require('sdk/panel');
 var iofile = require('sdk/io/file');
 var need_update = true;
 
-var topics = ["Bag Lunch", "Crackpot", "Dust", "Galaxies/Galaxy Scale", "Instrumentation",
+var topics = ["Bag Lunch", "Computational Methods", "Crackpot", "Dust", "Galaxies/Galaxy Scale", "Instrumentation",
 	    "Planets/Brown Dwarfs", "Pre-MS Stars", "Star Formation", "Stellar Clusters/Populations",
         "High E./X-Rays", "ISM/HII/PDR", "Disks", "Feedback SF/AGN", "Other Stars / Binaries", "Other"];
 
